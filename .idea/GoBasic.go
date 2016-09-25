aed702800540fe504906bf5f598703e50872bd8d
Will Hogan <willhogan11@hotmail.com>
2016-09-25 01:09:30 +0100

    Added struct and pointer samples

--- .idea/GoBasic.go
+++ .idea/GoBasic.go
@@ -10,6 +10,12 @@ func addNumbers(num1, num2 int)(int) {
 	return result
 }
 
+type person struct {
+	name string
+	age int
+}
+
+
 func main() {
 	fmt.Println("This is a test")
 	fmt.Println("The result is: ", addNumbers(12, 15))
@@ -19,5 +25,19 @@ func main() {
 		sum += i
 	}
 	fmt.Println(sum)
-}
 
+
+	// Struct Exmaple
+	var P person
+	P.name, P.age = "Will", 39
+	fmt.Println(P)
+
+
+	// Pointers Example
+	var num int = 5 //  Iniatialise num to 5
+	var ptr *int // Declare an int pointer
+	ptr = &num // ptr assigned the value of num
+
+	fmt.Println("Value of memory address that pointer is pointing to : ", &ptr) // Prints the Address that's been stored, ie the address of num
+	fmt.Println("Actual value at location of what pointer is pointing to (Dereferencing) : ", *ptr) // Prints the actual value stored by inital variable num, (AKA Dereferencing)
+}
